0ba75fe5717eaa28ca95a6b89c579679ed0da70d
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 19:15:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+take_a_number(array)
+def katz_deli =[]
+  puts "the line is currently empty"
+  counter += "0,1,2,3,4,5,6,7...1000"
+  now_serving(katz_deli)= "3"
+  puts "there is nobody witing to be served!"
+end 
