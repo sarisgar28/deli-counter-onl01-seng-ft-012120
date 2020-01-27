0d85bd603c8a1148eb2007ee6a2dcdebc05a45d9
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 19:15:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ take_a_number(array)
 def katz_deli =[]
   puts "the line is currently empty"
   counter += "0,1,2,3,4,5,6,7...1000"
-  now_serving = "3"
+  now_serving(katz_deli)= "3"
   puts "there is nobody witing to be served!"
 end 
